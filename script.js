077f8ca3cfa66508df0b4eaf6ac4fb0671e200b3
Wojo <matt.wojtkun@gmail.com>
2017-06-05 11:42:20 -0400

    made functioning clock

---
+++ script.js
@@ -0,0 +1,24 @@
+window.setInterval('updateClock()', 1000);
+
+var hrDisplay = document.getElementById('hours-display');
+var mnDisplay = document.getElementById('minutes-display');
+var scDisplay = document.getElementById('seconds-display');
+
+function updateClock(){
+  var time = new Date();
+
+  var currentHours = time.getHours();
+  var currentMinutes = time.getMinutes();
+  var currentSeconds = time.getSeconds();
+
+  currentHours = (currentHours < 10 ? "0" : "") + currentHours;
+  currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+  currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+  console.log(currentHours, currentMinutes, currentSeconds);
+
+  hrDisplay.innerHTML = currentHours;
+  mnDisplay.innerHTML = currentMinutes;
+  scDisplay.innerHTML = currentSeconds;
+
+}
