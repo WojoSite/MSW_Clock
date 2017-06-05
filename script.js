2ede6b29fdff056823a2b956e9acb6eac24f9a99
Wojo <matt.wojtkun@gmail.com>
2017-06-05 12:48:15 -0400

    made bg color change according to hex-time

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ window.setInterval('updateClock()', 1000);
 var hrDisplay = document.getElementById('hours-display');
 var mnDisplay = document.getElementById('minutes-display');
 var scDisplay = document.getElementById('seconds-display');
+var colorDisplay = document.getElementById("container");
 
 function updateClock(){
   var time = new Date();
-
   var currentHours = time.getHours();
   var currentMinutes = time.getMinutes();
   var currentSeconds = time.getSeconds();
@@ -15,10 +15,18 @@ function updateClock(){
   currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
   currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
 
-  console.log(currentHours, currentMinutes, currentSeconds);
+  // console.log(currentHours, currentMinutes, currentSeconds);
 
   hrDisplay.innerHTML = currentHours;
   mnDisplay.innerHTML = currentMinutes;
   scDisplay.innerHTML = currentSeconds;
+
+
+
+  var hexColor = (currentHours + currentMinutes + currentSeconds).toString();
+
+  // console.log(hexColor);
+
+  colorDisplay.style.backgroundColor = "#"+hexColor;
 
 }
