7b19677fd912060c23371948d7d44581c70cac6b
Wojo <matt.wojtkun@gmail.com>
2017-06-05 13:03:47 -0400

    made style changes

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
 window.setInterval('updateClock()', 1000);
-
-var hrDisplay = document.getElementById('hours-display');
-var mnDisplay = document.getElementById('minutes-display');
-var scDisplay = document.getElementById('seconds-display');
 var colorDisplay = document.getElementById("container");
+var clockDisplay = document.getElementById("clock-display");
 
 function updateClock(){
   var time = new Date();
-  var currentHours = time.getHours();
-  var currentMinutes = time.getMinutes();
-  var currentSeconds = time.getSeconds();
+  var hr = time.getHours();
+  var mn = time.getMinutes();
+  var sc = time.getSeconds();
 
-  currentHours = (currentHours < 10 ? "0" : "") + currentHours;
-  currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
-  currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+  hr = (hr < 10 ? "0" : "") + hr;
+  mn = (mn < 10 ? "0" : "") + mn;
+  sc = (sc < 10 ? "0" : "") + sc;
 
-  // console.log(currentHours, currentMinutes, currentSeconds);
+  // console.log(hr, mn, sc);
 
-  hrDisplay.innerHTML = currentHours;
-  mnDisplay.innerHTML = currentMinutes;
-  scDisplay.innerHTML = currentSeconds;
-
-
-
-  var hexColor = (currentHours + currentMinutes + currentSeconds).toString();
+  var hexColor = (hr + mn + sc).toString();
 
   // console.log(hexColor);
 
   colorDisplay.style.backgroundColor = "#"+hexColor;
+  clockDisplay.innerHTML = "#"+hexColor;
 
 }
